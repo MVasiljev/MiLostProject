218827ec8e807564fb6d14786a84a037cf443f0f
Milan Vasiljev <103666285+MVasiljev@users.noreply.github.com>
2025-04-10 08:49:41 +0200

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,5 @@
-// global.d.ts
-// This file declares global augmentations for TypeScript
-
-// Make this file a module
 export {};
 
-// Declare global augmentations
 declare global {
   interface Window {
     wasmModule: any;
